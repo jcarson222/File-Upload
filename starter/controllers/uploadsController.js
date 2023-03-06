f75749b46d687bf7ca0738616a5ae1af05410375
Jesse Carson <jmcarson222@gmail.com>
2023-03-06 15:14:18 -0600

    controllers

---
+++ starter/controllers/uploadsController.js
@@ -0,0 +1,7 @@
+const { StatusCodes } = require("StatusCodes");
+
+const uploadProductImage = async (req, res) => {
+  res.send("upload product image");
+};
+
+module.exports = { uploadProductImage };
